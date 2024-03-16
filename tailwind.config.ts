3a128b7a5578ffcf8af8fad50936871c7a654481
woojung007 <eeeeve007@gmail.com>
2024-03-16 23:14:26 +0900

    feat: layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        dark_blue: "#272a4b",
+
+        gray: {
+          f1: "#f1f1f1",
+          cc: "#ccc",
+        },
+      },
     },
   },
   plugins: [],
